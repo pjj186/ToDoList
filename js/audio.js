d40b42747b2d16eb95be7767120b968c00412651
Jong jae Park <pjj186@naver.com>
2021-01-23 06:48:08 +0900

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -2,7 +2,7 @@ const MUSIC_ON = "music_click";
 const bgm = document.querySelector(".fa-compact-disc");
 var audio1 = new Audio("audio/sparkle.mp3");
 audio1.loop = false; // 반복재생하지 않음
-audio1.volume = 0.02; // 음량 설정
+audio1.volume = 0.05; // 음량 설정
 
 document.querySelector(".fa-compact-disc").addEventListener("click", function () {
   if (audio1.paused) {
